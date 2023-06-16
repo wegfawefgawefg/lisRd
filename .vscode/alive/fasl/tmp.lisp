3e52322411d5cb5cb1706d79b95bec0b9a1fa849
wegfawefgaewfg <668es218pur@gmail.com>
2023-06-17 07:51:35 +0900

    k

---
+++ .vscode/alive/fasl/tmp.lisp
@@ -0,0 +1,18 @@
+(begin
+ ; (print 3)
+ ; (def dog 40)
+ ; (def ^2 (* x x))
+ (* 4 (+ (+ dog 3) 2))
+ ; (list (+ 1 1) (+ 2 2) (* 2 3) (expt 2 3))
+ ; (def a (list 1 2 3 4))
+ ; (print a)
+ ; (car a)
+ ; (cdr a)
+ ; (max a)
+
+ ; ; (map + a)
+ ; (def squared
+ ;     (fn (x) (* x x))
+ ; )
+ ; (squared 4)
+)
